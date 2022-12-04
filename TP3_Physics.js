ba317dc11ea608be6eacc54ae43f469ceb7bb41a
Vanessa-T-Soucy <vanessa.soucy@hotmail.com>
2022-12-03 20:36:40 -0500

    Update TP3_Physics.js

--- TP3_Physics.js
+++ TP3_Physics.js
@@ -1,6 +1,7 @@
 const appleMass = 0.075;
 
 TP3.Physics = {
+<<<<<<< Updated upstream
 	initTree: function (rootNode) {
 
 		this.computeTreeMass(rootNode);
@@ -61,3 +62,174 @@ TP3.Physics = {
 		}
 	}
 }
+=======
+  initTree: function (rootNode) {
+    this.computeTreeMass(rootNode);
+
+    var stack = [];
+    stack.push(rootNode);
+
+    while (stack.length > 0) {
+      var currentNode = stack.pop();
+      for (var i = 0; i < currentNode.childNode.length; i++) {
+        stack.push(currentNode.childNode[i]);
+      }
+
+      currentNode.bp0 = currentNode.p0.clone();
+      currentNode.bp1 = currentNode.p1.clone();
+      currentNode.rp0 = currentNode.p0.clone();
+      currentNode.rp1 = currentNode.p1.clone();
+      currentNode.vel = new THREE.Vector3();
+      currentNode.strength = currentNode.a0;
+    }
+  },
+
+  computeTreeMass: function (node) {
+    var mass = 0;
+
+    for (var i = 0; i < node.childNode.length; i++) {
+      mass += this.computeTreeMass(node.childNode[i]);
+    }
+    mass += node.a1;
+    if (node.appleIndices !== null) {
+      mass += appleMass;
+    }
+    node.mass = mass;
+
+    return mass;
+  },
+
+  // applyRotation: function (parentNode, originalP0, originalP1) {
+
+
+  //   if (parentNode.parentNode != null) {
+  //     [originalP0, originalP1] = this.applyRotation(parentNode.parentNode, originalP0, originalP1)
+  //   }
+  //   originalP0.sub(parentNode.p0Initial).applyMatrix4(parentNode.matrixTransformation).add(parentNode.p0Initial);
+  //   originalP1.sub(parentNode.p0Initial).applyMatrix4(parentNode.matrixTransformation).add(parentNode.p0Initial);
+
+  //   return [originalP0, originalP1]
+  // },
+
+  applyForces: function (node, dt, time) {
+    var u = Math.sin(1 * time) * 4;
+    u += Math.sin(2.5 * time) * 2;
+    u += Math.sin(5 * time) * 0.4;
+
+    var v = Math.cos(1 * time + 56485) * 4;
+    v += Math.cos(2.5 * time + 56485) * 2;
+    v += Math.cos(5 * time + 56485) * 0.4;
+
+    // Ajouter le vent
+    node.vel.add(new THREE.Vector3(u / Math.sqrt(node.mass), 0, v / Math.sqrt(node.mass)).multiplyScalar(dt));
+
+    // Ajouter la gravite
+    node.vel.add(new THREE.Vector3(0, -node.mass, 0).multiplyScalar(dt));
+
+
+    // TODO: Projection du mouvement, force de restitution et amortissement de la velocite
+
+
+    let parentNode = node.parentNode;
+    let originalP0 = node.p0Initial.clone();
+    let originalP1 = node.p1Initial.clone();
+    //propagation 
+    while (parentNode != null) {
+      originalP0.sub(parentNode.p0Initial).applyMatrix4(parentNode.matrixTransformation).add(parentNode.p0Initial);
+      originalP1.sub(parentNode.p0Initial).applyMatrix4(parentNode.matrixTransformation).add(parentNode.p0Initial);
+      parentNode = parentNode.parentNode;
+    }
+
+    let moveP1 = originalP1.clone().sub(originalP0).applyMatrix4(node.matrixTransformation).add(originalP0);
+
+    //Préparer les variables pour le cacule de movement
+    let movement = node.vel.clone();
+    //appliquer le mouvement sans conserver la longueur de la branche
+    let nonConservedP1 = new THREE.Vector3().addVectors(moveP1, movement);
+
+    // calculer l'angle causé par le mouvement
+    let initialDirection = new THREE.Vector3().subVectors(moveP1, originalP0);
+    let nonConservedDirection = new THREE.Vector3().subVectors(nonConservedP1, originalP0);
+    let crossNonConserved = initialDirection.clone().cross(nonConservedDirection);
+    let angle = initialDirection.angleTo(nonConservedDirection);
+
+    // Appliquer l'angle de rotation sur la branche initial pour garder la taille de celle-ci
+    // le quaternion
+    // let quaternionRotation = new THREE.Quaternion().setFromAxisAngle(crossNonConserved, angle);
+
+    // la matrice de rotation
+    // let matrixRotationTransformation = new THREE.Matrix4().makeRotationFromQuaternion(quaternionRotation);
+    let matrixRotationTransformation = new THREE.Matrix4().makeRotationAxis(crossNonConserved, angle);
+
+    let vectorBranch = new THREE.Vector3().subVectors(moveP1, originalP0);
+    let trueNewP1 = vectorBranch.clone().applyMatrix4(matrixRotationTransformation);
+
+    trueNewP1.add(originalP0);
+
+    // Calculer la vrai vélocité causé par l'Angle
+    // node.vel = new THREE.Vector3().subVectors(trueNewP1, moveP1).divideScalar(dt);
+    let angleSquared = Math.pow(angle, 2);
+
+    // quaternionRotation = new THREE.Quaternion().setFromAxisAngle(crossNonConserved, -angleSquared);
+    // let matrixRotation = new THREE.Matrix4().makeRotationFromQuaternion(quaternionRotation);
+    let matrixRotation = new THREE.Matrix4().makeRotationAxis(crossNonConserved, angleSquared);
+
+
+
+
+
+    let restitutionVectorBranch = new THREE.Vector3().subVectors(originalP0, trueNewP1);
+    let restitutionPoint = restitutionVectorBranch.clone().applyMatrix4(matrixRotation);
+    restitutionPoint.add(originalP0);
+
+
+    let restitution = new THREE.Vector3().subVectors(originalP1, restitutionPoint);
+    let scalar = node.a0 * 1000;
+    restitution.multiplyScalar(scalar);
+
+    node.vel.add(restitution);
+
+    // facteur d'amortissement
+    node.vel.multiplyScalar(0.7);
+
+
+    // //On réaplique la nouvelle vélocité au résultat final 
+    // //Préparer les variables pour le cacule de movement
+    // movement = node.vel.clone().multiplyScalar(dt);
+    // //appliquer le mouvement sans conserver la longueur de la branche
+    // nonConservedP1 = new THREE.Vector3().addVectors(moveP1, movement);
+
+    // // calculer l'angle causé par le mouvement
+    // initialDirection = new THREE.Vector3().subVectors(moveP1, originalP0).normalize();
+    // nonConservedDirection = new THREE.Vector3().subVectors(nonConservedP1, originalP0).normalize();
+    // crossNonConserved = initialDirection.clone().cross(nonConservedDirection);
+    // angle = initialDirection.angleTo(nonConservedDirection);
+    // vectorBranch = new THREE.Vector3().subVectors(moveP1, originalP0);
+
+    // quaternionRotation = new THREE.Quaternion().setFromAxisAngle(crossNonConserved, -angleSquared);
+    // // let matrixRotation = new THREE.Matrix4().makeRotationFromQuaternion(quaternionRotation);
+    // matrixRotationTransformation = new THREE.Matrix4().makeRotationAxis(crossNonConserved, angle)
+    // trueNewP1 = vectorBranch.clone().applyMatrix4(matrixRotationTransformation);
+
+    // trueNewP1.add(originalP0);
+
+
+
+    node.matrixTransformation = matrixRotationTransformation.clone();
+    // let TotalMovement = new THREE.Vector3().subVectors(trueNewP1, originalP);
+    // node.vectorTransformationParenthood =   new THREE.Matrix4().multiplyMatrices(node.nodeParent.vectorTransformationParenthood)
+
+    // new THREE.Matrix4().makeTranslation(TotalMovement.x, TotalMovement.y, TotalMovement.z);
+
+
+    node.p0 = originalP0.clone();
+    node.p1 = trueNewP1.clone();
+
+
+    // Appel recursif sur les enfants
+    node.childNode.forEach(childNode => {
+      this.applyForces(childNode, dt, time);
+    });
+  },
+}
+>>>>>>> Stashed changes
